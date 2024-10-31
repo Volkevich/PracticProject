44e597f1f863c3a399a06a34c6793d982b502900
Vitaly Volkevich <87798074+Volkevich@users.noreply.github.com>
2024-10-31 11:56:10 +0300

    add forms.py

--- newsproject/news/forms.py
+++ newsproject/news/forms.py
@@ -1,24 +1,54 @@
+import re
+
 from django import forms
+from django.core.exceptions import ValidationError
 
-from news.models import Category
+from news.models import News
 
 
-class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(
-        attrs={
-            'class': 'form-control'
+# class NewsForm(forms.Form):
+#     title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(
+#         attrs={
+#             'class': 'form-control'
+#         }
+#     ))
+#     content = forms.CharField(label='Содержание', required=False, widget=forms.Textarea(
+#         attrs={
+#             'class': 'form-control',
+#             'rows': 5
+#         }
+#     ))
+#     is_published = forms.BooleanField(label='Опубликовать?', initial=True)
+#     category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория',
+#                                       empty_label='Выберите категорию', widget=forms.Select(
+#             attrs={
+#                 'class': 'form-control'
+#             }
+#         ))
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = ['title', 'content', 'category', 'is_published']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
         }
-    ))
-    content = forms.CharField(label='Содержание', required=False, widget=forms.Textarea(
-        attrs={
-            'class': 'form-control',
-            'rows': 5
-        }
-    ))
-    is_published = forms.BooleanField(label='Опубликовать?', initial=True)
-    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория',
-                                      empty_label='Выберите категорию', widget=forms.Select(
-            attrs={
-                'class': 'form-control'
-            }
-        ))
+
+    def __init__(self, *args, **kwargs):
+        super(NewsForm, self).__init__(*args, **kwargs)
+        self.fields['category'].empty_label = "Выберите категорию"
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'^\d+$', title):
+            raise ValidationError('Название не должно содержать только цифры')
+        if re.match(r'^\d+', title):
+            raise ValidationError('Название не должно начинаться с цифры')
+        return title
+
+    def clean_is_published(self):
+        if self.cleaned_data['is_published']:
+            return self.cleaned_data['is_published']
+        raise ValidationError('Статус публикации должен быть подтверждён')
--- newsproject/news/views.py
+++ newsproject/news/views.py
@@ -16,8 +16,9 @@ def index(request):
     }
     return render(request, 'news/index.html', context)
 
+
 def get_category(request, category_id):
-    news = News.objects.filter(category_id = category_id)
+    news = News.objects.filter(category_id=category_id)
     # category = Category.objects.get(pk=category_id)
     category = get_object_or_404(Category, pk=category_id)
     context = {
@@ -30,15 +31,16 @@ def get_category(request, category_id):
 def view_news(request, news_id):
     # news_item = News.objects.get(pk=news_id)
     news_item = get_object_or_404(News, pk=news_id)
-    return render(request,'news/view_news.html', context={'news_item': news_item})
+    return render(request, 'news/view_news.html', context={'news_item': news_item})
 
 
 def add_news(request):
     if request.method == 'POST':
         form = NewsForm(request.POST)
         if form.is_valid():
-            News.objects.create(**form.cleaned_data)
-            return redirect('home')
+            #news = News.objects.create(**form.cleaned_data)
+            news = form.save()
+            return redirect(news)
     else:
         form = NewsForm()
     return render(request, 'news/add_news.html', context={'form': form})
