4e48bf5c604a5a367d8f11496e158a543904d65d
Vitaly Volkevich <87798074+Volkevich@users.noreply.github.com>
2024-10-31 09:46:49 +0300

    add news_add

--- newsproject/news/forms.py
+++ newsproject/news/forms.py
@@ -5,7 +5,7 @@ from news.models import Category
 
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=120)
-    content = forms.CharField()
-    is_published = forms.BooleanField(required=False)
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=150, label='Название')
+    content = forms.CharField(label='Содержание', required=False)
+    is_published = forms.BooleanField(label='Опубликовать?')
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Выберите категорию')
