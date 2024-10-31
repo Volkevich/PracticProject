abdfd697460bcf90d31cd9682f775b4efe590aaa
Vitaly Volkevich <87798074+Volkevich@users.noreply.github.com>
2024-10-31 11:07:48 +0300

    add news_add full

--- newsproject/news/forms.py
+++ newsproject/news/forms.py
@@ -1,11 +1,24 @@
-
 from django import forms
 
 from news.models import Category
 
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150, label='Название')
-    content = forms.CharField(label='Содержание', required=False)
-    is_published = forms.BooleanField(label='Опубликовать?')
-    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Выберите категорию')
+    title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(
+        attrs={
+            'class': 'form-control'
+        }
+    ))
+    content = forms.CharField(label='Содержание', required=False, widget=forms.Textarea(
+        attrs={
+            'class': 'form-control',
+            'rows': 5
+        }
+    ))
+    is_published = forms.BooleanField(label='Опубликовать?', initial=True)
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория',
+                                      empty_label='Выберите категорию', widget=forms.Select(
+            attrs={
+                'class': 'form-control'
+            }
+        ))
